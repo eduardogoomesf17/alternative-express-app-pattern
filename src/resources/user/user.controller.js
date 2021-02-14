07779143a086b4a54cca7077f23cafb98e43474b
Eduardo Gomes <eduardogoomesf@hotmail.com>
2021-02-14 11:45:16 -0300

    Moving user controller functions to from routes to a separated file

---
+++ src/resources/user/user.controller.js
@@ -0,0 +1,57 @@
+const status = require('http-status');
+
+class UserController {
+
+  constructor(UserService, UserRepository, User) {
+    this.userService = new UserService(UserRepository, User);
+  }
+
+  async createUser(request, response, next) {
+    try {
+      const userData = request.body;
+
+      const user = await this.userService.createUser(userData);
+
+      return response.status(status.OK).json({ result: user });
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async getUsers(request, response, next) {
+    try {
+      const users = await this.userService.getUsers();
+      
+      return response.status(status.OK).json({ result: users, message: status['200_MESSAGE'] });
+    } catch (error) { 
+      return next(error);
+    }
+  }
+
+  async getUserById(request, response) {
+    try {
+      const userId = request.params.id;
+
+      const user = await this.userService.getUserById(userId);
+      
+      return response.status(status.OK).json({ result: user, message: status['200_MESSAGE'] });
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async getUserByEmail(request, response) {
+    try {
+      const userEmail = request.params.email;
+
+      const user = await this.userService.getUserByEmail(userEmail);
+      
+      return response.status(status.OK).json({ result: user, message: status['200_MESSAGE'] });
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+}
+
+module.exports = UserController;
--- src/resources/user/user.routes.js
+++ src/resources/user/user.routes.js
@@ -1,61 +1,22 @@
 const express = require('express');
-const status = require('http-status');
 
+const UserController = require('./user.controller');
 const User = require('./user.model');
 const UserRepository = require('./user.repository');
 const UserService = require('./user.service');
 
 const routes = express.Router();
 
-const userService = new UserService(UserRepository, User);
+const userController = new UserController(UserService, UserRepository, User);
 
 module.exports = (app) => {
   app.use('/users', routes);
   
-  routes.get('/', async (request, response, next) => {
-    try {
-      const users = await userService.getUsers();
-      
-      return response.status(status.OK).json({ result: users, message: status['200_MESSAGE'] });
-    } catch (error) { 
-      return next(error);
-    }
-  });
+  routes.get('/', userController.getUsers);
 
-  routes.get('/:id', async (request, response) => {
-    try {
-      const userId = request.params.id;
+  routes.get('/:id', userController.getUserById);
 
-      const user = await userService.getUserById(userId);
-      
-      return response.status(status.OK).json({ result: user, message: status['200_MESSAGE'] });
-    } catch (error) {
-      return next(error);
-    }
-  });
+  routes.get('/email/:email', userController.getUserByEmail);
 
-  routes.get('/email/:email', async (request, response) => {
-    try {
-      const userEmail = request.params.email;
-
-      const user = await userService.getUserByEmail(userEmail);
-      
-      return response.status(status.OK).json({ result: user, message: status['200_MESSAGE'] });
-    } catch (error) {
-      return next(error);
-    }
-  });
-
-  routes.post('/', async (request, response, next) => {
-    try {
-      const userData = request.body;
-
-      const user = await userService.createUser(userData);
-
-      return response.status(status.OK).json({ result: user });
-    } catch (error) {
-      return next(error);
-    }
-  })
-  
+  routes.post('/', userController.createUser);
 }
