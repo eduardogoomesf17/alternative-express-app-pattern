e55a6d530424d722a1c00e5910f6e84c1ab2c644
Eduardo Gomes <eduardogoomesf@hotmail.com>
2021-02-17 13:55:00 -0300

    Using token validator middleware to private some user module's route

--- src/resources/user/user.routes.js
+++ src/resources/user/user.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const UserController = require('./user.controller');
+const { authorizationMiddlewares } = require('../../api/middlewares/index');
 
 const routes = express.Router();
 
@@ -8,18 +9,38 @@ const userController = new UserController();
 
 module.exports = (app) => {
   app.use('/users', routes);
-  
-  routes.get('/', userController.getUsers);
-
-  routes.get('/:id', userController.getUserById);
-
-  routes.get('/email/:email', userController.getUserByEmail);
 
   routes.post('/', userController.createUser);
 
-  routes.patch('/:id', userController.updateUser);
+  routes.patch(
+    '/:id', 
+    authorizationMiddlewares.attachCurrenctUser,
+    userController.updateUser
+  );
 
-  routes.delete('/:id', userController.deleteUser);
+  routes.delete(
+    '/:id', 
+    authorizationMiddlewares.attachCurrenctUser,
+    userController.deleteUser
+  );
+  
+  routes.get(
+    '/', 
+    authorizationMiddlewares.attachCurrenctUser,
+    userController.getUsers
+  );
+
+  routes.get(
+    '/:id', 
+    authorizationMiddlewares.attachCurrenctUser,
+    userController.getUserById
+  );
+
+  routes.get(
+    '/email/:email', 
+    authorizationMiddlewares.attachCurrenctUser,
+    userController.getUserByEmail
+  );
 
   routes.post('/auth', userController.authenticateUser);
 }
